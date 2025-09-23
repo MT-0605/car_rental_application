b035e735244310b4c291e30512ab0f4dd44b9c40
MT-0605 <meettank652006@gmail.com>
2025-09-24 01:18:47 +0530

    add multer for image uploading

--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-const { addCar, getCarById, getAllCars } = require('../controllers/carController');
+const { addCar, getCarById, getAllCars, getNextAvailableDate } = require('../controllers/carController');
 
 // Multer config for image upload
 const storage = multer.diskStorage({
