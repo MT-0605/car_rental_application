3bbad06532379ceea0106a75bb809e50ec8c5c23
MT-0605 <meettank652006@gmail.com>
2025-09-24 01:43:42 +0530

    added protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,6 +19,7 @@ import ContactSupport from './pages/ContactSupport'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import TermsOfService from './pages/TermsOfService'
 import MyBookings from './pages/MyBookingPage'
+import ProtectedRoute from './utils/ProtectedRoute'
 
 function App() {
   const location = useLocation();
@@ -32,21 +33,21 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/list-car' element={<ListCar />} />
-        <Route path='/cars' element={<Cars />} />
-        <Route path='/cars/:id' element={<CarDetails />} />
-        <Route path='/my-bookings' element={<MyBookings />} />
-        <Route path='/learn-more' element={<LearnMore />} />
-        <Route path='/help' element={<Help />} />
-        <Route path='/contact' element={<ContactSupport />} />
-        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-        <Route path='/terms-of-service' element={<TermsOfService />} />
+        <Route path='/home' element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path='/list-car' element={<ProtectedRoute><ListCar /></ProtectedRoute>} />
+        <Route path='/cars' element={<ProtectedRoute><Cars /></ProtectedRoute>} />
+        <Route path='/cars/:id' element={<ProtectedRoute><CarDetails /></ProtectedRoute>} />
+        <Route path='/my-bookings' element={<ProtectedRoute><MyBookings /></ProtectedRoute>} />
+        <Route path='/learn-more' element={<ProtectedRoute><LearnMore /></ProtectedRoute>} />
+        <Route path='/help' element={<ProtectedRoute><Help /></ProtectedRoute>} />
+        <Route path='/contact' element={<ProtectedRoute><ContactSupport /></ProtectedRoute>} />
+        <Route path='/privacy-policy' element={<ProtectedRoute><PrivacyPolicy /></ProtectedRoute>} />
+        <Route path='/terms-of-service' element={<ProtectedRoute><TermsOfService /></ProtectedRoute>} />
 
-        <Route path='/admin' element={<Admin />} />
-        <Route path='/admin/bookings' element={<AdminBookings />} />
-        <Route path='/admin/cars' element={<AdminCars />} />
-        <Route path='/admin/users' element={<AdminUsers />} />
+        <Route path='/admin' element={<ProtectedRoute><Admin /></ProtectedRoute>} />
+        <Route path='/admin/bookings' element={<ProtectedRoute><AdminBookings /></ProtectedRoute>} />
+        <Route path='/admin/cars' element={<ProtectedRoute><AdminCars /></ProtectedRoute>} />
+        <Route path='/admin/users' element={<ProtectedRoute><AdminUsers /></ProtectedRoute>} />
       </Routes>
       {!isLoginPage && !isSignupPage && !isAdmin && <Footer />}
     </>
